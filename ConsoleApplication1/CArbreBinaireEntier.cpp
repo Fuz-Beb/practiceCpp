c60709145690f03cbb34592f3720bb756c0aa9e4
Bebo <pierrick.bobet@cryptolab.net>
2017-03-06 19:43:40 +0100

    Avancement CArbreBinaire

--- ConsoleApplication1/CArbreBinaireEntier.cpp
+++ ConsoleApplication1/CArbreBinaireEntier.cpp
@@ -1,5 +1,5 @@
-#include "stdio.h"
 #include "CArbreBinaireEntier.h"
+#include <stdio.h>
 
 CArbreBinaireEntier::CArbreBinaireEntier()
 {
@@ -10,40 +10,51 @@ CArbreBinaireEntier::CArbreBinaireEntier()
 CArbreBinaireEntier::CArbreBinaireEntier(CArbreBinaireEntier & pABEArbreBinaire)
 {
 	iABEElement = pABEArbreBinaire.iABEElement;
+	
+	if (pABEArbreBinaire.pABEFilsGauche == NULL)
+		pABEFilsGauche = NULL;
+	else
+		pABEFilsGauche = new CArbreBinaireEntier(*pABEArbreBinaire.pABEFilsGauche);
+
+	if (pABEArbreBinaire.pABEFilsDroit == NULL)
+		pABEFilsDroit = NULL;
+	else
+		pABEFilsDroit = new CArbreBinaireEntier(*pABEArbreBinaire.pABEFilsDroit);
 }
 
 CArbreBinaireEntier::~CArbreBinaireEntier()
 {
-
+	delete pABEFilsDroit;
+	delete pABEFilsGauche;
 }
 
 int CArbreBinaireEntier::ABELire_Element()
 {
-
+	return iABEElement;
 }
 
 void CArbreBinaireEntier::ABEModifier_Element(CArbreBinaireEntier * pABEArbreBinaire, unsigned int uiElement)
 {
-
+	iABEElement = uiElement;
 }
 
 CArbreBinaireEntier * CArbreBinaireEntier::ABELire_FilsGauche()
 {
-
+	return pABEFilsGauche;
 }
 
 
 void CArbreBinaireEntier::ABEModifier_FilsGauche(CArbreBinaireEntier * pABEArbreBinaire)
 {
-
+	pABEFilsGauche = pABEArbreBinaire;
 }
 
 CArbreBinaireEntier * CArbreBinaireEntier::ABELire_FilsDroit()
 {
-
+	return pABEFilsGauche;
 }
 
 void CArbreBinaireEntier::ABEModifier_FilsDroit(CArbreBinaireEntier * pABEArbreBinaire)
 {
-
+	pABEFilsDroit = pABEArbreBinaire;
 }
